cf0283e121e84f2602f5bff3274065c91737e9db
andre <andre@DESKTOP-QH76U9C>
2023-06-27 15:21:00 +0300

    Добавьте файлы проекта.

---
+++ WpfAppPP/Classes/DataBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppPP.Classes
+{
+    public class DataBase
+    {
+        public static KochnevPPLogistickEntities connect;
+    }
+}
